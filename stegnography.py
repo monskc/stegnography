d9de7b8439c574601ee133fee8dee38d8a4d99db
monskc <97909962+monskc@users.noreply.github.com>
2025-06-23 14:54:56 +0000

    main file

---
+++ stegnography.py
@@ -0,0 +1,57 @@
+from PIL import Image
+ 
+def xor_encrypt_decrypt(text, key):
+    return ''.join(chr(ord(c) ^ ord(key[i % len(key)])) for i, c in enumerate(text))
+
+def text_to_bin(text):
+    return ''.join(format(ord(c), '08b') for c in text)
+
+def bin_to_text(binary):
+    chars = [binary[i:i+8] for i in range(0, len(binary), 8)]
+    return ''.join(chr(int(c, 2)) for c in chars)
+ 
+def encode_image(image_path, secret_message, key, output_image_path):
+    img = Image.open(image_path)
+    img = img.convert('RGB')
+    pixels = list(img.getdata())
+
+    encrypted = xor_encrypt_decrypt(secret_message + '~', key)
+    binary_data = text_to_bin(encrypted)
+
+    if len(binary_data) > len(pixels) * 3:
+        print("❌ Message too long for this image.")
+        return
+
+    new_pixels = []
+    data_index = 0
+
+    for pixel in pixels:
+        r, g, b = pixel
+        if data_index < len(binary_data):
+            r = (r & ~1) | int(binary_data[data_index])
+            data_index += 1
+        if data_index < len(binary_data):
+            g = (g & ~1) | int(binary_data[data_index])
+            data_index += 1
+        if data_index < len(binary_data):
+            b = (b & ~1) | int(binary_data[data_index])
+            data_index += 1
+        new_pixels.append((r, g, b))
+
+    img.putdata(new_pixels)
+    img.save(output_image_path)
+    print(f"[✔] Encrypted image saved as '{output_image_path}'")
+
+def decode_image(image_path, key):
+    img = Image.open(image_path)
+    img = img.convert('RGB')
+    pixels = list(img.getdata())
+
+    binary_data = ''
+    for pixel in pixels:
+        for channel in pixel:
+            binary_data += str(channel & 1)
+
+    binary_chars = [binary_data[i:i+8] for i in range(0, len(binary_data), 8)]
+    extracted = ''
+    for byte in binary_chars:
